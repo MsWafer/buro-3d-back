72849498f2dd40101499bad32205e888094e52f3
MsWafer <phalosof@gmail.com>
2021-01-26 14:34:26 +0300

    auth ebanina

--- middleware/onLaunch.js
+++ middleware/onLaunch.js
@@ -11,7 +11,28 @@ module.exports = async (
   res
 ) => {
   try {
-    await ooa(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, scopes);
+    await Axios({
+      method: "POST",
+      url: "https://developer.api.autodesk.com/authentication/v1/authenticate",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+      },
+      data: querystring.stringify({
+        client_id: FORGE_CLIENT_ID,
+        client_secret: FORGE_CLIENT_SECRET,
+        grant_type: "client_credentials",
+        scope: scopes,
+      }),
+    })
+      .then((response) => {
+        process.env.access_token = response.data.access_token;
+      //   console.log(response);
+        // res.redirect("/api/forge/datamanagement/bucket/create");
+      })
+      .catch((error) => {
+        console.log(error);
+        res.send("Failed to authenticate");
+      });
     await Axios({
       method: "POST",
       url: "https://developer.api.autodesk.com/authentication/v1/authenticate",
